ae0bdda3e30a6dcf1b450ba70999855fa6fa034c
Sara <bracewell.sara@gmail.com>
2018-01-09 07:44:11 -0600

    I added an if/else statement to use only gifs that had an appropriate rating and increased the query limit so it had enough gifs to review so that there were always 10 gifs on the browser

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ var topics = ["dog", "cat", "rabbit", "turtle", "fish", "squirrel", "deer", "sku
 var animalBtn;
 var animalImage;
 
+var rating;
+
 function renderButtons() {
 //want to pull data-animal from variable
   
@@ -45,8 +47,9 @@ function displayImages() {
     var animal = $(this).attr("data-animal");
     console.log("this: " + this);
     console.log("animal: " + animal);
-    //
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=WtL7a88I6lDSvslFMP2JlDT1WvGXcUET&limit=10";
+    
+    //I increased the limit from 10 to 25 as I want to get 10 g/y/pg rated gifs and needed more available to look through, so I was not left with less then 10 gifs on the browser (rabbits gifs had a lot of r rated gifs and didn't want them on site so my children could watch)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=WtL7a88I6lDSvslFMP2JlDT1WvGXcUET&limit=25";
 
     //
     $.ajax({
@@ -61,8 +64,12 @@ function displayImages() {
       //
       var results = response.data;
 
+      //x is how many total gifs we want to go to the browser, if one of the gifs is r or pg-13 rated then it should go back to respnse and look at another gif and add 1 to x so that it will give a total of 10 gifs displayed
+      //without this it would only go through the length of the array which when the limit on the query was 10 it would put less than 10 appropriate gifs on the browswer
+      var x = 10;
+      
       //want to see 10 gifs each time button pressed
-      for (var i = 0; i < results.length; i++) {
+      for (var i = 0; i < x; i++) {
         console.log("results.length: " + results.length);
         //create variable with a div to put image and rating in
         var gifDiv = $("<div class='item float-left'>");
@@ -70,36 +77,49 @@ function displayImages() {
         //variable created to hold rating of specific gif
         var rating = results[i].rating;
 
-        //put rating on html in paragraph
-        var p = $("<p>").text("Rating: " + rating);
+        //I do not want any gifs with r ratings posted
+        //create an if statement that checks rating, if it is not r or pg-13 rating ok so can use it 
+        if (rating !== "r" && rating !== "pg-13") {
+          //put rating on html in paragraph
+          var p = $("<p>").text("Rating: " + rating);
 
-        //create variable called animalImage and put into image element
-        var animalImage = $("<img>");
+          //create variable called animalImage and put into image element
+          var animalImage = $("<img>");
 
-        //give animalImage src and image information
-        animalImage.attr("src", results[i].images.fixed_height_still.url);
+          //give animalImage src and image information
+          animalImage.attr("src", results[i].images.fixed_height_still.url);
 
-        //give animalImage data-state information to allow image to be in still state
-        animalImage.attr("data-state", "still");
+          //give animalImage data-state information to allow image to be in still state
+          animalImage.attr("data-state", "still");
 
-        //give animalImage data-still image information to allow image to be in still state
-        animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+          //give animalImage data-still image information to allow image to be in still state
+          animalImage.attr("data-still", results[i].images.fixed_height_still.url);
 
-        //give animalImage data-animate image information so when clicked it will play gif
-        animalImage.attr("data-animate", results[i].images.fixed_height.url);
+          //give animalImage data-animate image information so when clicked it will play gif
+          animalImage.attr("data-animate", results[i].images.fixed_height.url);
 
-        //add a class to animalImage
-        animalImage.addClass("gif");
-
-
-        //for each image/paragraph prepend to div 
-        gifDiv.prepend(animalImage);
-        gifDiv.prepend(p);
+          //add a class to animalImage
+          animalImage.addClass("gif");
 
 
-        //put image and image div on browser
-        $("#gifs-appear-here").prepend(gifDiv);
-        
+          //for each image/paragraph prepend to div 
+          gifDiv.prepend(animalImage);
+          gifDiv.prepend(p);
+
+
+          //put image and image div on browser
+          $("#gifs-appear-here").prepend(gifDiv);
+
+          //want 10 images displayed so add image here not in for var as that would only go through 10 gifs, but if ratings were r or pg-13 it would count them and not show them, displaying most of the time less than 10 gifs
+          
+        }
+
+        //if r or pg-13 rating go back to beginning of loop and get another gif and add another to x, so we get a total of 10 gifs
+        else {
+          x++;
+        }
+
+          
       }
 
       //click function on gif to allow user to play the gif
